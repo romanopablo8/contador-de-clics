5352575e7cd6c74d538b6c6841c8f18749703b16
romanopablo8 <romanopablo83@gmail.com>
2022-09-13 15:21:13 -0300

    primer commit

---
+++ src/Componentes/Contador.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "../hojas-de-estilo/contador.css"
+
+function Contador ( {numClics} ) {
+
+    return (
+       <div className="contador">
+          {numClics}
+       </div>
+
+    );
+}
+
+
+
+export default Contador;
